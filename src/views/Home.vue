8624d8e7bf643089e41809604278f3519c093c20
Cindy Viviana Rodriguez R <cinvir15@gmail.com>
2020-11-09 09:38:02 +0100

    antes de la notificacion de busqueda

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,22 +6,22 @@
       h1.title.is-4 Estacion Platzi
 
     .field
-      label.label Nombre
+      label.has-text-weight-bold.is-size-7 Nombre
       .control
-        input.input(type="text",placeholder="Text input")
+        input.input(type="text")
 
     .field
       label.label Apellido
       .control
-        input.input(type="text",placeholder="Text input")
+        input.input(type="text")
 
     .field
       label.label Nombre
       .control
-        input.input(type="text",placeholder="Text input")
+        input.input(type="text")
 
     .column.is-offset-8
-      .button ENVIAR
+      button.button ENVIAR
 
 </template>
 
@@ -33,29 +33,20 @@ export default {
   components: {}
 }
 </script>
-<style  scoped>
+<style lang="scss" scoped>
+@import "../scss/my-style.scss";
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap');
 .container{
   color: #212121;
-font-family: 'Roboto', sans-serif;
+  font-family: 'Roboto', sans-serif;
 }
 h1{
   color: #00BFD6 ;
 }
-input{
-  border: none;
-  border-bottom-color: lawngreen;
-  box-shadow: none;
-  border-bottom: solid #FF9300 .01em ;
-  border-radius: 0;
-  padding-bottom: 0rem;
-  padding-top: 0rem;
 
-}
-label {
-  color: #FF9300;
-  font-size: 0.7rem;
-  margin-bottom: 0em;
+ label {
+ color: $naranja-platzi;
+ font-size:  0.75rem;
 }
 button{
   background: #00BFD6 ;
